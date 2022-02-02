c4dc9b14c1b90b0551868cd6d3130ff00b9446e4
harish786harry <harish786harry@gmail.com>
2022-02-02 15:59:12 +0530

    second commit

---
+++ Programming/src/programs/Vowel.java
@@ -0,0 +1,19 @@
+package programs;
+import java.util.Scanner;
+class Vowel {
+	public static void main(String[] args) {
+		Scanner ip=new Scanner(System.in);
+		System.out.println("Enter an Alphabet");
+		char x=ip.next().charAt(0);
+		if(x=='a' || x=='e' || x=='i' || x=='o' || x=='u'
+			||	x=='A' || x=='E' || x=='I' || x=='O' || x=='U')
+		{
+			System.out.println(x+" is a Vowel");
+		}
+		else
+		{
+			System.out.println(x+" is a consonant");
+		}
+	}
+
+}
